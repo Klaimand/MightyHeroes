ad84fda35444b1f99afed11431492caa07065c7b
Clempep <clempagnier@yahoo.fr>
2022-02-15 12:45:38 +0100

    j'en peux plus des quaternions

--- ProjectA/Assets/KLD/KLD_Scripts/KLD_PlayerAim.cs
+++ ProjectA/Assets/KLD/KLD_Scripts/KLD_PlayerAim.cs
@@ -10,6 +10,14 @@ public class KLD_PlayerAim : MonoBehaviour
     [SerializeField] KLD_ZombieList zombieList;
     [SerializeField] Transform defaultTarget = null;
 
+
+    [SerializeField] bool isAiming = false;
+    [SerializeField, ReadOnly] Vector2 inputAimVector = Vector2.zero;
+    Vector3 inputAimVector3 = Vector3.zero;
+    Vector3 worldAimVector3 = Vector3.zero;
+
+
+
     [SerializeField, ReadOnly] KLD_ZombieAttributes selectedZombie = null;
 
     [InlineEditor(InlineEditorObjectFieldModes.Foldout)]
@@ -20,7 +28,7 @@ public class KLD_PlayerAim : MonoBehaviour
     Vector3 debugPosa = Vector3.zero;
     Vector3 debugPosb = Vector3.zero;
 
-    KLD_PlayerAttributes playerAttributes;
+    [SerializeField, ReadOnly] KLD_PlayerAttributes playerAttributes;
 
     void Awake()
     {
@@ -36,6 +44,8 @@ public class KLD_PlayerAim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Aim();
+
         selectedZombie = aimBehavior.GetZombieToTarget(zombieList.GetZombies(), playerAttributes);
 
         targetPos = selectedZombie != null ?
@@ -46,6 +56,25 @@ public class KLD_PlayerAim : MonoBehaviour
         transform.LookAt(targetPos, Vector3.up);
 
         DrawSelectedLine();
+    }
+
+    void Aim()
+    {
+        if (isAiming && inputAimVector.sqrMagnitude > 0.05f)
+        {
+            inputAimVector3.x = inputAimVector.x;
+            inputAimVector3.y = 0f;
+            inputAimVector3.z = inputAimVector.y;
+
+            inputAimVector3 = playerAttributes.transform.rotation * inputAimVector3;
+        }
+        else
+        {
+            inputAimVector3 = playerAttributes.transform.forward;
+        }
+
+        playerAttributes.worldAimDirection.x = inputAimVector3.x;
+        playerAttributes.worldAimDirection.y = inputAimVector3.z;
     }
 
     void DrawSelectedLine()
@@ -62,6 +91,10 @@ public class KLD_PlayerAim : MonoBehaviour
         if (playerAttributes == null)
         {
             playerAttributes = new KLD_PlayerAttributes { transform = this.transform };
+        }
+        else if (playerAttributes.transform == null)
+        {
+            playerAttributes.transform = transform;
         }
     }
 }
