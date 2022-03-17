c50d8400317cba77845b176d8c9e065348d94f6e
Clément Pagnier <clempagnier@yahoo.fr>
2022-03-17 20:30:07 +0100

    joysticks fonctionnent bien

--- ProjectA/Assets/KLD/KLD_Scripts/Inputs/KLD_TouchInputs.cs
+++ ProjectA/Assets/KLD/KLD_Scripts/Inputs/KLD_TouchInputs.cs
@@ -7,44 +7,28 @@ using UnityEngine.EventSystems;
 
 public class KLD_TouchInputs : MonoBehaviour
 {
+    [System.Serializable]
+    class Joystick
+    {
+        public int padding = 100;
+        public CanvasGroup canvasGroup;
+        public RectTransform firstTouchCircle;
+        public RectTransform touchCircle;
+        public RectTransform stickCircle;
+        [HideInInspector] public Vector2 rawPosition;
+        [HideInInspector] public Vector2 rawVector;
+        [HideInInspector] public Vector2 rawCappedVector;
+        [ReadOnly] public Vector2 normalizedVector;
+    }
+
+    [SerializeField] Joystick[] joysticks;
+
     [SerializeField] bool overrideScreenSize = false;
     [SerializeField, ShowIf("overrideScreenSize")] Vector2Int overridenScreenSize = Vector2Int.zero;
 
     int height;
     int width;
 
-    [Header("UI REFERENCES")]
-    [SerializeField] RectTransform L_firstTouchCircle;
-    [SerializeField] RectTransform R_firstTouchCircle;
-    [SerializeField] RectTransform L_touchCircle;
-    [SerializeField] RectTransform R_touchCircle;
-    [SerializeField] RectTransform L_stickCircle;
-    [SerializeField] RectTransform R_stickCircle;
-
-
-    Vector2 leftTouch_rawPosition = Vector2.zero;
-    Vector2 rightTouch_rawPosition = Vector2.zero;
-
-    Vector2 leftTouchRawVector = Vector2.zero;
-    Vector2 rightTouchRawVector = Vector2.zero;
-
-    Vector2 leftTouchRawCappedVector = Vector2.zero;
-    Vector2 rightTouchRawCappedVector = Vector2.zero;
-
-    [SerializeField, ReadOnly] Vector2 leftTouchNormalizedVector = Vector2.zero;
-    [SerializeField, ReadOnly] Vector2 rightTouchNormalizedVector = Vector2.zero;
-
-
-    [Header("Joysticks")]
-    [SerializeField] int leftStickPadding = 200;
-    [SerializeField] int rightStickPadding = 200;
-
-    [SerializeField] float rightStickMaxTimeToShoot = 0.2f;
-
-    bool isRightStickPressed = false;
-    float curRightStickPressTime = 0f;
-
-    // Start is called before the first frame update
     void Start()
     {
         if (!overrideScreenSize)
@@ -59,12 +43,16 @@ public class KLD_TouchInputs : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         UpdateInputs();
         ProcessVectors();
     }
+
+    //local vars
+    Touch curTouch;
+    bool isLeftTouch;
+    int joyIndex;
 
     void UpdateInputs()
     {
@@ -72,49 +60,33 @@ public class KLD_TouchInputs : MonoBehaviour
         {
             for (int i = 0; i < Input.touchCount; i++)
             {
-                Touch curTouch = Input.GetTouch(i);
+                curTouch = Input.GetTouch(i);
 
-                bool isLeftTouch = curTouch.position.x < width / 2;
+                isLeftTouch = curTouch.position.x < width / 2;
+
+                joyIndex = isLeftTouch ? 0 : 1;
 
                 if (curTouch.phase == TouchPhase.Began)
                 {
                     if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(i).fingerId))
                     {
-                        if (isLeftTouch)
-                        {
-                            leftTouch_rawPosition = curTouch.position;
-                            L_firstTouchCircle.anchoredPosition = leftTouch_rawPosition;
-                            L_touchCircle.anchoredPosition = curTouch.position;
-                        }
-                        else
-                        {
-                            isRightStickPressed = true;
-                            rightTouch_rawPosition = curTouch.position;
-                            R_firstTouchCircle.anchoredPosition = rightTouch_rawPosition;
-                            R_touchCircle.anchoredPosition = curTouch.position;
-                        }
+                        joysticks[joyIndex].rawPosition = curTouch.position;
+                        joysticks[joyIndex].firstTouchCircle.anchoredPosition = joysticks[joyIndex].rawPosition;
+                        joysticks[joyIndex].touchCircle.anchoredPosition = curTouch.position;
+
+                        joysticks[joyIndex].canvasGroup.alpha = 1f;
                     }
                 }
                 else if (curTouch.phase == TouchPhase.Moved)
                 {
-                    if (isLeftTouch)
-                    {
-                        leftTouchRawVector = curTouch.position - leftTouch_rawPosition;
-                        L_touchCircle.anchoredPosition = curTouch.position;
-                    }
-                    else
-                    {
-                        rightTouchRawVector = curTouch.position - rightTouch_rawPosition;
-                        R_touchCircle.anchoredPosition = curTouch.position;
-                    }
+                    joysticks[joyIndex].rawVector = curTouch.position - joysticks[joyIndex].rawPosition;
+                    joysticks[joyIndex].touchCircle.anchoredPosition = curTouch.position;
                 }
                 else if (curTouch.phase == TouchPhase.Ended)
                 {
-                    if (!isLeftTouch)
-                    {
-                        isRightStickPressed = false;
-                        print("aa");
-                    }
+                    joysticks[joyIndex].rawPosition = Vector2.zero;
+                    joysticks[joyIndex].rawVector = Vector2.zero;
+                    joysticks[joyIndex].canvasGroup.alpha = 0f;
                 }
             }
         }
@@ -122,59 +94,17 @@ public class KLD_TouchInputs : MonoBehaviour
 
     void ProcessVectors()
     {
-        //left
-        leftTouchRawCappedVector = leftTouchRawVector.sqrMagnitude > leftStickPadding * leftStickPadding ?
-        leftTouchRawVector.normalized * leftStickPadding :
-        leftTouchRawVector;
+        for (int i = 0; i < joysticks.Length; i++)
+        {
+            joysticks[i].rawCappedVector =
+            joysticks[i].rawVector.sqrMagnitude > joysticks[i].padding * joysticks[i].padding ?
+            joysticks[i].rawVector.normalized * joysticks[i].padding :
+            joysticks[i].rawVector;
 
-        L_stickCircle.anchoredPosition = leftTouch_rawPosition + leftTouchRawCappedVector;
+            joysticks[i].stickCircle.anchoredPosition = joysticks[i].rawPosition + joysticks[i].rawCappedVector;
 
-        leftTouchNormalizedVector = leftTouchRawCappedVector / leftStickPadding;
-
-
-        //right
-        rightTouchRawCappedVector = rightTouchRawVector.sqrMagnitude > rightStickPadding * rightStickPadding ?
-        rightTouchRawVector.normalized * rightStickPadding :
-        rightTouchRawVector;
-
-        R_stickCircle.anchoredPosition = rightTouch_rawPosition + rightTouchRawCappedVector;
-
-        rightTouchNormalizedVector = rightTouchRawCappedVector / rightStickPadding;
+            joysticks[i].normalizedVector = joysticks[i].rawCappedVector / joysticks[i].padding;
+        }
     }
 
-
-
-
-
-
-    /*
-        void ObsUpdateInputs()
-        {
-            if (Input.touchCount > 0)
-            {
-                Touch touch = Input.GetTouch(0);
-
-                if (touch.phase == TouchPhase.Began)
-                {
-                    if (touch.position.x < width / 2)
-                    {
-                        if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
-                        {
-                            //firstTouchPos = touch.position;
-
-                            L_firstTouchCircle.anchoredPosition = touch.rawPosition;
-                        }
-                    }
-                }
-                else if (touch.phase == TouchPhase.Moved)
-                {
-                    leftTouchRawVector = touch.position - touch.rawPosition;
-
-                    L_touchCircle.anchoredPosition = touch.position;
-                }
-
-                //print(leftTouchRawVector);
-            }
-        }
-    */
 }
