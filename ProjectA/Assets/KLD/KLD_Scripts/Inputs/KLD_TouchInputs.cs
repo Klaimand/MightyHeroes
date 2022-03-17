ba2d726fc251c0bec91b67f8497f871759ac3dd1
Clément Pagnier <clempagnier@yahoo.fr>
2022-03-17 19:22:51 +0100

    avancements qui fonctionnent, je vais clean

--- ProjectA/Assets/KLD/KLD_Scripts/Inputs/KLD_TouchInputs.cs
+++ ProjectA/Assets/KLD/KLD_Scripts/Inputs/KLD_TouchInputs.cs
@@ -7,10 +7,6 @@ using UnityEngine.EventSystems;
 
 public class KLD_TouchInputs : MonoBehaviour
 {
-    Vector2 leftTouchRawVector;
-    Vector2 rightTouchRawVector;
-
-
     [SerializeField] bool overrideScreenSize = false;
     [SerializeField, ShowIf("overrideScreenSize")] Vector2Int overridenScreenSize = Vector2Int.zero;
 
@@ -19,9 +15,34 @@ public class KLD_TouchInputs : MonoBehaviour
 
     [Header("UI REFERENCES")]
     [SerializeField] RectTransform L_firstTouchCircle;
-    [SerializeField] RectTransform L_touchCircle;
     [SerializeField] RectTransform R_firstTouchCircle;
+    [SerializeField] RectTransform L_touchCircle;
     [SerializeField] RectTransform R_touchCircle;
+    [SerializeField] RectTransform L_stickCircle;
+    [SerializeField] RectTransform R_stickCircle;
+
+
+    Vector2 leftTouch_rawPosition = Vector2.zero;
+    Vector2 rightTouch_rawPosition = Vector2.zero;
+
+    Vector2 leftTouchRawVector = Vector2.zero;
+    Vector2 rightTouchRawVector = Vector2.zero;
+
+    Vector2 leftTouchRawCappedVector = Vector2.zero;
+    Vector2 rightTouchRawCappedVector = Vector2.zero;
+
+    [SerializeField, ReadOnly] Vector2 leftTouchNormalizedVector = Vector2.zero;
+    [SerializeField, ReadOnly] Vector2 rightTouchNormalizedVector = Vector2.zero;
+
+
+    [Header("Joysticks")]
+    [SerializeField] int leftStickPadding = 200;
+    [SerializeField] int rightStickPadding = 200;
+
+    [SerializeField] float rightStickMaxTimeToShoot = 0.2f;
+
+    bool isRightStickPressed = false;
+    float curRightStickPressTime = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -41,7 +62,8 @@ public class KLD_TouchInputs : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ObsUpdateInputs();
+        UpdateInputs();
+        ProcessVectors();
     }
 
     void UpdateInputs()
@@ -52,20 +74,24 @@ public class KLD_TouchInputs : MonoBehaviour
             {
                 Touch curTouch = Input.GetTouch(i);
 
-                bool isLeftTouch = curTouch.rawPosition.x < width / 2;
-                print(curTouch.rawPosition);
+                bool isLeftTouch = curTouch.position.x < width / 2;
 
                 if (curTouch.phase == TouchPhase.Began)
                 {
-                    if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+                    if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(i).fingerId))
                     {
                         if (isLeftTouch)
                         {
-                            L_firstTouchCircle.anchoredPosition = curTouch.rawPosition;
+                            leftTouch_rawPosition = curTouch.position;
+                            L_firstTouchCircle.anchoredPosition = leftTouch_rawPosition;
+                            L_touchCircle.anchoredPosition = curTouch.position;
                         }
                         else
                         {
-                            R_firstTouchCircle.anchoredPosition = curTouch.rawPosition;
+                            isRightStickPressed = true;
+                            rightTouch_rawPosition = curTouch.position;
+                            R_firstTouchCircle.anchoredPosition = rightTouch_rawPosition;
+                            R_touchCircle.anchoredPosition = curTouch.position;
                         }
                     }
                 }
@@ -73,45 +99,82 @@ public class KLD_TouchInputs : MonoBehaviour
                 {
                     if (isLeftTouch)
                     {
-                        leftTouchRawVector = curTouch.position - curTouch.rawPosition;
+                        leftTouchRawVector = curTouch.position - leftTouch_rawPosition;
                         L_touchCircle.anchoredPosition = curTouch.position;
                     }
                     else
                     {
-                        rightTouchRawVector = curTouch.position - curTouch.rawPosition;
+                        rightTouchRawVector = curTouch.position - rightTouch_rawPosition;
                         R_touchCircle.anchoredPosition = curTouch.position;
                     }
                 }
-            }
-        }
-    }
-
-    void ObsUpdateInputs()
-    {
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-
-            if (touch.phase == TouchPhase.Began)
-            {
-                if (touch.position.x < width / 2)
+                else if (curTouch.phase == TouchPhase.Ended)
                 {
-                    if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+                    if (!isLeftTouch)
                     {
-                        //firstTouchPos = touch.position;
-
-                        L_firstTouchCircle.anchoredPosition = touch.rawPosition;
+                        isRightStickPressed = false;
+                        print("aa");
                     }
                 }
             }
-            else if (touch.phase == TouchPhase.Moved)
-            {
-                leftTouchRawVector = touch.position - touch.rawPosition;
-
-                L_touchCircle.anchoredPosition = touch.position;
-            }
-
-            //print(leftTouchRawVector);
         }
     }
+
+    void ProcessVectors()
+    {
+        //left
+        leftTouchRawCappedVector = leftTouchRawVector.sqrMagnitude > leftStickPadding * leftStickPadding ?
+        leftTouchRawVector.normalized * leftStickPadding :
+        leftTouchRawVector;
+
+        L_stickCircle.anchoredPosition = leftTouch_rawPosition + leftTouchRawCappedVector;
+
+        leftTouchNormalizedVector = leftTouchRawCappedVector / leftStickPadding;
+
+
+        //right
+        rightTouchRawCappedVector = rightTouchRawVector.sqrMagnitude > rightStickPadding * rightStickPadding ?
+        rightTouchRawVector.normalized * rightStickPadding :
+        rightTouchRawVector;
+
+        R_stickCircle.anchoredPosition = rightTouch_rawPosition + rightTouchRawCappedVector;
+
+        rightTouchNormalizedVector = rightTouchRawCappedVector / rightStickPadding;
+    }
+
+
+
+
+
+
+    /*
+        void ObsUpdateInputs()
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                if (touch.phase == TouchPhase.Began)
+                {
+                    if (touch.position.x < width / 2)
+                    {
+                        if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+                        {
+                            //firstTouchPos = touch.position;
+
+                            L_firstTouchCircle.anchoredPosition = touch.rawPosition;
+                        }
+                    }
+                }
+                else if (touch.phase == TouchPhase.Moved)
+                {
+                    leftTouchRawVector = touch.position - touch.rawPosition;
+
+                    L_touchCircle.anchoredPosition = touch.position;
+                }
+
+                //print(leftTouchRawVector);
+            }
+        }
+    */
 }
