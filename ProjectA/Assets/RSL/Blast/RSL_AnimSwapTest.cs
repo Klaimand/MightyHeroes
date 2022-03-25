9d880f05dcbe75672d47d75839838762fe530eeb
damotime <remisutralanut@gmail.com>
2022-03-25 11:13:25 +0100

    le tool d'anim

--- ProjectA/Assets/RSL/Blast/RSL_AnimSwapTest.cs
+++ ProjectA/Assets/RSL/Blast/RSL_AnimSwapTest.cs
@@ -28,39 +28,70 @@ public class RSL_AnimSwapTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        WeaponAnimation();
+
+        CharacterAnimation();
+
+    }
+
+    void WeaponAnimation()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            animator.SetBool("Unarmed", false);
+            aiming = true;
+            animator.SetBool("Aiming", true);
+
+        }
+        else
+        {
+            aiming = false;
+            animator.SetBool("Aiming", false);
+        }
+
+        if (Input.GetButton("Fire1") && aiming == true) 
+        {
+
+            shooting = true;
+            animator.SetBool("Shooting", true);
+        }
+        else
+        {
+            shooting = false;
+            animator.SetBool("Shooting", false);
+        }
+
+        if (Input.GetButton("Fire2"))
+        {
+            animator.SetBool("Unarmed", false);
+            animator.SetTrigger("Reloading");
+            aiming = false;
+            shooting = false;
+        }
+
+        if (animator.GetCurrentAnimatorStateInfo(1).IsName("Blast_GrenadeThrow"))
+        {
+            animator.SetBool("Unarmed", true);
+        }
+        else animator.SetBool("Unarmed", false);
+    }
+    void CharacterAnimation()
+    {
         if (Input.GetKey(KeyCode.Z))
             running = true;
-
         else running = false;
-
         if (running == false)
             animator.SetBool("Running", false);
         if (running == true)
             animator.SetBool("Running", true);
 
 
-        if (Input.GetKey(KeyCode.Alpha1))
-            aiming = true;
-
-        else aiming = false;
-        if (aiming == false)
-            animator.SetBool("Aiming", false);
-        if (aiming == true)
-            animator.SetBool("Aiming", true);
-
-
-        if (Input.GetKey(KeyCode.Alpha2))
-            shooting = true;
-
-        else shooting = false;
-        if (shooting == false)
-            animator.SetBool("Shooting", false);
-        if (shooting == true)
-            animator.SetBool("Shooting", true);
-
-        if (Input.GetKeyDown(KeyCode.R))
-            animator.SetTrigger("Reloading");
         if (Input.GetKeyDown(KeyCode.E))
+        {
+            //animator.SetBool("Unarmed", true);
             animator.SetTrigger("Grenade");
+        }
+        //else animator.SetBool("Unarmed", false);
     }
+
 }
