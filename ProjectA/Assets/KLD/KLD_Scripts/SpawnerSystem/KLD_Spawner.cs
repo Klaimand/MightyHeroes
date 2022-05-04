c02836a3103384e8dbc524b5f931298076e238f9
ClempepGit <clempaaaaaaaiiu@gmail.com>
2022-05-04 19:46:17 +0200

    avancée sur le spawner system - a corriger : le onvalidate du spawner

--- ProjectA/Assets/KLD/KLD_Scripts/SpawnerSystem/KLD_Spawner.cs
+++ ProjectA/Assets/KLD/KLD_Scripts/SpawnerSystem/KLD_Spawner.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 public class KLD_Spawner : MonoBehaviour
 {
+    [SerializeField] KLD_SpawnerStep[] steps;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +18,64 @@ public class KLD_Spawner : MonoBehaviour
     {
 
     }
+
+    void OnValidate()
+    {
+        if (steps == null || steps.Length == 0) return;
+
+        steps[0].timeToActivate = 0f;
+
+        foreach (var step in steps)
+        {
+            if (step.enemiesProbas == null || step.enemiesProbas.Length == 0) continue;
+
+            step.enemiesProbas[step.enemiesProbas.Length - 1].proba = 1f;
+
+            if (step.timeToActivate < 0f) step.timeToActivate = 0f;
+
+            step.OnValidate();
+        }
+    }
 }
 
+[System.Serializable]
 public class KLD_SpawnerStep
 {
     public float timeToActivate = 0f;
 
+    public KLD_EnemyProba[] enemiesProbas;
+
+    public void OnValidate()
+    {
+        for (int y = 0; y < 2; y++)
+        {
+            for (int i = 0; i < enemiesProbas.Length; i++)
+            {
+                if (i != 0 && enemiesProbas[i - 1].proba > enemiesProbas[i].proba)
+                {
+                    enemiesProbas[i].proba = enemiesProbas[i - 1].proba;
+                }
+                /*
+                if (i < enemiesProbas.Length - 1 && enemiesProbas[i + 1].proba < enemiesProbas[i].proba)
+                {
+                    enemiesProbas[i].proba = enemiesProbas[i + 1].proba;
+                }
+                */
+            }
+        }
+    }
 }
 
-//public class KLD_Enemy
+[System.Serializable]
+public class KLD_EnemyProba
+{
+    [Range(0f, 1f)] public float proba = 0.5f;
+
+    [ValueDropdown("EnemiesKeys")]
+    public string enemy = "Swarmer";
+
+    public int enemyPerSpawn = 3;
+
+    string[] EnemiesKeys = { "Swarmer", "Kamikaze", "Alpha" };
+
+}
