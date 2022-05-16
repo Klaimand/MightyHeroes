7cdc21e063aed24b2a9f95796b69913f44c2ac18
Xavier LEZZOCHE <xavier.lezzoche@gmail.com>
2022-05-16 16:48:47 +0200

    Upgrade des armes

--- ProjectA/Assets/XL/XL_Scripts/UI/XL_WeaponDetailsMenu.cs
+++ ProjectA/Assets/XL/XL_Scripts/UI/XL_WeaponDetailsMenu.cs
@@ -23,6 +23,9 @@ public class XL_WeaponDetailsMenu : MonoBehaviour
     [SerializeField] private Transform magazineSizeXScaler;
     [SerializeField] private float scalerMagazineSizeMax;
 
+    [Header("Upgrade Button")]
+    [SerializeField] private TMP_Text upgradeText;
+
     public int selectedWeapon;
 
     public void Select(int idx)
@@ -34,18 +37,36 @@ public class XL_WeaponDetailsMenu : MonoBehaviour
 
         selectedWeapon = idx;
 
+        DisplayWeaponInfo(idx);
+
+        weaponInfos[idx].Activate();
+    }
+
+    private void DisplayWeaponInfo(int idx)
+    {
         //Initialise Text
         weaponInfos[idx].DisplayLevel();
         damageText.text = weaponInfos[idx].GetDamage().ToString();
         rpmText.text = weaponInfos[idx].GetRPM().ToString();
         magazineSizeText.text = weaponInfos[idx].GetMagazine().ToString();
+        upgradeText.text = weaponInfos[idx].weaponAttributes.weaponAttributes[weaponInfos[idx].GetLevel()].experienceToReach.ToString(); //Aled
 
         //Initialise Bar
         damageXScaler.localScale = new Vector3(weaponInfos[idx].GetDamage() / scalerDamageMax, damageXScaler.localScale.y, damageXScaler.localScale.z);
         rpmXScaler.localScale = new Vector3(weaponInfos[idx].GetRPM() / scalerRPMMax, rpmXScaler.localScale.y, rpmXScaler.localScale.z);
         magazineSizeXScaler.localScale = new Vector3(weaponInfos[idx].GetMagazine() / scalerMagazineSizeMax, magazineSizeXScaler.localScale.y, magazineSizeXScaler.localScale.z);
 
-        weaponInfos[idx].Activate();
+    }
 
+    public void UpgradeWeapon() 
+    {
+        if (PlayerPrefs.GetInt("SoftCurrency") > weaponInfos[selectedWeapon].weaponAttributes.weaponAttributes[weaponInfos[selectedWeapon].GetLevel()].experienceToReach) 
+        {
+            PlayerPrefs.SetInt(weaponInfos[selectedWeapon].weaponAttributes.weaponName, weaponInfos[selectedWeapon].weaponAttributes.level + 1);
+            weaponInfos[selectedWeapon].weaponAttributes.level++;
+            PlayerPrefs.SetInt("SoftCurrency", PlayerPrefs.GetInt("SoftCurrency") - weaponInfos[selectedWeapon].weaponAttributes.weaponAttributes[weaponInfos[selectedWeapon].GetLevel()].experienceToReach);
+            Debug.Log("Currency left : " + PlayerPrefs.GetInt("SoftCurrency"));
+            DisplayWeaponInfo(selectedWeapon);
+        }
     }
 }
