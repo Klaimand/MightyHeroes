729957c74b2c2cb1bd2a4ab385e10b6034bbcd32
ClempepGit <clempaaaaaaaiiu@gmail.com>
2022-05-17 10:16:55 +0200

    avancement du leveling des spells

--- ProjectA/Assets/KLD/KLD_Scripts/Spells/KLD_SpellHeal.cs
+++ ProjectA/Assets/KLD/KLD_Scripts/Spells/KLD_SpellHeal.cs
@@ -5,8 +5,26 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "newSpellHeal", menuName = "KLD/Spell/Heal", order = 0)]
 public class KLD_SpellHeal : KLD_Spell
 {
+    [System.Serializable]
+    class HealZoneAttributes
+    {
+        public float duration = 5f;
+        public float tickDuration = 0.2f;
+        public float healingPerTick = 20f;
+    }
+
+    [SerializeField] List<HealZoneAttributes> levels = new List<HealZoneAttributes>();
+
+    GameObject curHealZone;
+
     public override void ActivateSpell(Vector3 direction, Transform pos, int characterLevel)
     {
-        XL_Pooler.instance.PopPosition("SimoHeal", pos.position, pos);
+        curHealZone = XL_Pooler.instance.PopPosition("SimoHeal", pos.position, pos);
+
+        curHealZone.GetComponent<KLD_HealZone>().SetHealZoneAttributes(
+            levels[characterLevel].duration,
+            levels[characterLevel].tickDuration,
+            levels[characterLevel].healingPerTick
+        );
     }
 }
