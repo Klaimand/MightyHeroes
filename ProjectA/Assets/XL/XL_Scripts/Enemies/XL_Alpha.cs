3303baa2fdb06660c52c27c94727489b4cfde899
Xavier LEZZOCHE <xavier.lezzoche@gmail.com>
2022-05-19 14:08:50 +0200

    Summoner debug + scene du menu

--- ProjectA/Assets/XL/XL_Scripts/Enemies/XL_Alpha.cs
+++ ProjectA/Assets/XL/XL_Scripts/Enemies/XL_Alpha.cs
@@ -8,9 +8,19 @@ public class XL_Alpha : XL_Enemy
     [SerializeField] private float summonCooldown;
     [SerializeField] private int nbEnemiesSummoned;
     [SerializeField] private float summonDistance;
+
+    [Header("Missile Launchers")]
+    [SerializeField] private Transform[] cannonPos;
+    private int missileLaunched = 0;
+
+    [Header("Projectile")]
     [SerializeField] private float projectilespeed;
     [SerializeField] private float projectileTravelTime;
     [SerializeField] private float fireRate;
+    [SerializeField] private int projectileDamage;
+    [SerializeField] private float projectileExplosionRadius;
+    private GameObject impactZone;
+
     GameObject vfx;
 
     [SerializeField] private Animator animator;
@@ -22,27 +32,30 @@ public class XL_Alpha : XL_Enemy
     }
 
     private const float g = 9.81f; //gravity
-    private const float h = 0.5f; //starting height
+    private float h = 5f; //starting height
     private float distance; //shoot
     private float[] velocity;
 
     public override void Attack()
     {
-        projectile = XL_Pooler.instance.PopPosition("AlphaProjectile", shootDirection.normalized + transform.position);
-        projectile.GetComponent<XL_Projectile>().Initialize();
+        h = cannonPos[missileLaunched % cannonPos.Length].position.y;
+        projectile = XL_Pooler.instance.PopPosition("Summoner_Projectile", cannonPos[missileLaunched % cannonPos.Length].position);
+        projectile.GetComponent<XL_Projectile>().Initialize(projectileDamage, projectileExplosionRadius);
         //projectile.GetComponent<Rigidbody>().velocity = shootDirection * projectilespeed;
-        velocity = XL_Utilities.GetVelocity(h, distance - 1, projectileTravelTime); //-1 because the projectile is shoot 1 meter in front of the enemy
+        velocity = XL_Utilities.GetVelocity(h, distance, projectileTravelTime);
         projectile.GetComponent<Rigidbody>().velocity = new Vector3(velocity[0] * (shootDirection.x / distance), velocity[1], velocity[0] * (shootDirection.z / distance));
+        missileLaunched++;
     }
 
+    private int j;
     private Vector3 summonPosition;
-    private float angleOffset;
     IEnumerator SummonCoroutine(float t)
     {
+        j = 0;
         yield return new WaitForSeconds(t - 0.7f);
 
         animator.SetBool("Summoning", true);
-        vfx = XL_Pooler.instance.PopPosition("SummoningChargeVFX", transform.position);
+        vfx = XL_Pooler.instance.PopPosition("SummoningChargeVFX", transform.position + transform.up * 4);
 
         yield return new WaitForSeconds(0.7f);
 
@@ -50,21 +63,25 @@ public class XL_Alpha : XL_Enemy
         animator.SetBool("Spawning", true);
 
         XL_Pooler.instance.DePop("SummoningChargeVFX", vfx);
-        XL_Pooler.instance.PopPosition("SpawningVFX", transform.position);
 
 
-        yield return new WaitForSeconds(0.6f);
-
-        angleOffset = 360 / nbEnemiesSummoned;
-        for (int i = 0; i < nbEnemiesSummoned; i++)
+        while (j < nbEnemiesSummoned)
         {
-            summonPosition = Quaternion.Euler(0, angleOffset * i, 0) * transform.forward * summonDistance;
-            //XL_GameManager.instance.AddEnemyAttributes(XL_Pooler.instance.PopPosition("Swarmer", summonPosition + transform.position).GetComponent<XL_Enemy>().GetZombieAttributes());
-            XL_Pooler.instance.PopPosition("Swarmer", summonPosition + transform.position);
-        }
+            
+            yield return new WaitForSeconds(0.85f);
+            vfx = XL_Pooler.instance.PopPosition("SpawningVFX", transform.position + transform.up * 4);
 
+            summonPosition = transform.forward * summonDistance;
+            //XL_GameManager.instance.AddEnemyAttributes(XL_Pooler.instance.PopPosition("Swarmer", summonPosition + transform.position).GetComponent<XL_Enemy>().GetZombieAttributes());
+            XL_Pooler.instance.PopPosition("Swarmer", summonPosition + transform.position)
+                .transform.LookAt(XL_GameManager.instance.players[0].transform.position);
+
+
+            
+            XL_Pooler.instance.DelayedDePop(0.8f, "SpawningVFX", vfx);
+            j++;
+        }
         animator.SetBool("Spawning", false);
-        XL_Pooler.instance.DePop("SpawningVFX", vfx);
         StartCoroutine(SummonCoroutine(t));
     }
 
@@ -103,9 +120,11 @@ public class XL_Alpha : XL_Enemy
         if (other.transform.CompareTag("Player") && canAttack)
         {
             //Debug.Log("Alpha is attacking");
+            impactZone = XL_Pooler.instance.PopPosition("Summoner_ImpactZone", other.transform.position);
             StartCoroutine(AttackCooldownCoroutine(fireRate));
-            shootDirection = (other.transform.position - transform.position);
-            distance = (new Vector3(other.transform.position.x, 0, other.transform.position.z) - new Vector3(transform.position.x, 0, transform.position.z)).magnitude;
+            shootDirection = (other.transform.position - cannonPos[missileLaunched % cannonPos.Length].position);
+            distance = (new Vector3(other.transform.position.x, 0, other.transform.position.z) - new Vector3(cannonPos[missileLaunched % (cannonPos.Length - 1)].position.x, 0, cannonPos[missileLaunched % (cannonPos.Length - 1)].position.z)).magnitude;
+            XL_Pooler.instance.DelayedDePop(projectileTravelTime, "Summoner_ImpactZone", impactZone);
             Attack();
         }
     }
